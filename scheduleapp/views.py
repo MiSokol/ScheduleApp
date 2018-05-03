628c78c77fed475a6c874c43b44795ff8541d4cd
Michail Sokolov <so-mi-ig@yandex.ru>
2018-05-03 19:35:22 +0300

    add urls handler

--- scheduleapp/views.py
+++ scheduleapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import User, Task
+
+def task_list(request):
+    return HttpResponse("Main Page")
+
+def login(request):
+    return HttpResponse("Login Page")
+
+def settings(request):
+    return HttpResponse("Settings Page")
