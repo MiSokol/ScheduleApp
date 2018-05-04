8e4795739d88b1b7c1e8036ba12cba019913e4fb
Danila Mishin <danila.mishin.2001@bk.ru>
2018-05-04 16:06:18 +0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
-
+var name = $("#name_inp").val(),
+      time = $("#time_inp").val(),
+      deadline = $("#deadline_inp").val();
 
 $(function(){
     $(".task").click(function() {
@@ -10,7 +12,7 @@ $(function(){
     });
 
     $("#add_task").click(function () {
-        addTask();
+        askTask();
     });
 });
 
@@ -31,7 +33,7 @@ function completeTask() {
   });
 };
 
-function addTask() {
+function askTask() {
   $( "#dialog-add-task" ).dialog({
     resizable: false,
     height: "auto",
@@ -39,6 +41,7 @@ function addTask() {
     modal: true,
     buttons: {
       "Да": function() {
+        addTask()
         $( this ).dialog( "close" );
       },
       "Орда": function() {
@@ -46,4 +49,30 @@ function addTask() {
       }
     }
   });
+}
+
+
+function addTask() {
+
+  console.log(name);
+
+  $( "#tasklist" ).append( "<div class='task'>" +
+        "<p class='name'>" + name + "</p>" +
+        "<p class='time'>" +  time + " минут </p>" +
+        "<p class='deadline'> Deadline: " + deadline + "</p>" +
+      "</div>" );
+
+  $(function(){
+    $(".task").click(function() {
+        completeTask();
+    });
+
+    $("#logout_btn").click(function () {
+        window.location.replace("logout")
+    });
+
+    $("#add_task").click(function () {
+        askTask();
+    });
+});
 }
