6b0fdc8fc133499c5ca32aa6edc5698c8ed079db
JungleTryne <danila.mishin.2001@gmail.com>
2018-05-04 13:42:37 +0400

    Added adaptive design

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,41 @@
-function openComplete() {
-  $( "#dialog" ).dialog({
-    resizable: false,
-    height: "auto",
-    width: 400,
-    modal: true,
-    buttons: {
-      "Да": function() {
-        $( this ).dialog( "close" );
-      },
-      "Орда": function() {
-        $( this ).dialog( "close" );
-      }
-    }
-  });
-}
 
-function openAdd() {
-    
-}
+
+$(function(){
+    $(".task").click(function() {
+        completeTask();
+    });
+});
+
+function completeTask() {
+  $( "#dialog-confirm" ).dialog({
+  resizable: false,
+  height: "auto",
+  width: 400,
+  modal: true,
+  buttons: {
+    "Да": function() {
+      $( this ).dialog( "close" );
+    },
+    "Орда": function() {
+      $( this ).dialog( "close" );
+    }
+  }
+});
+};
+
+
+  /*$( "#dialog" ).dialog({
+        resizable: false,
+        height: "auto",
+        width: 400,
+        modal: true,
+        buttons: {
+          "Да": function() {
+            $( this ).dialog( "close" );
+          },
+          "Орда": function() {
+            $( this ).dialog( "close" );
+          }
+        }
+      });
+    });*/
