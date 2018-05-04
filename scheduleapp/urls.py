5fb6efe1273f4eaa45aa14019df36a4b886b9fb6
Michail Sokolov <so-mi-ig@yandex.ru>
2018-05-04 12:33:33 +0300

    add functional auth system

--- scheduleapp/urls.py
+++ scheduleapp/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
 
     path("", views.task_list, name="task_list"),
     path("login/", views.login, name="login"),
+    path("check_login/", views.check_login, name="check_login"),
+    path("logout/", views.logout, name="logout"),
     path("settings/", views.settings, name="settings"),
 ]
--- scheduleapp/views.py
+++ scheduleapp/views.py
@@ -1,13 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import User, Task
 
-def task_list(request):
-    return HttpResponse("Main Page")
 
+def check_auth(request):
+    try:
+        cookies = request.session['login']
+        if len(User.objects.filter(login=cookies)) > 0:
+            return True
+        else:
+            return False
+    except KeyError:
+        return False
+
+
+#Authorization
 def login(request):
     return render(request, "scheduleapp/login_page.html")
 
+def check_login(request):
+    input_login = request.POST["login"]
+    if len(User.objects.filter(login=input_login)) > 0:
+        request.session['login'] = input_login
+        return redirect("/")
+    else:
+        return redirect("/login/")
+
+def logout(request):
+    request.session["login"] = "NULL"
+    return redirect("/login/")
+
+
+#Main
+def task_list(request):
+    if not check_auth(request):
+        return redirect("/login/")
+
+    return render(request, "scheduleapp/main_page.html")
+
 def settings(request):
+    if not check_auth(request):
+        return redirect("/login/")
+
     return HttpResponse("Settings Page")
